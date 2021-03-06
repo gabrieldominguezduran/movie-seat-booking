7921b4870b40008af31ce0be906ea4aef37d4677
Gabriel Dominguez Duran <gabrieldominguezduran@gmail.com>
2021-03-06 20:58:50 +0100

    Add exchange rate API

--- script.js
+++ script.js
@@ -3,10 +3,44 @@ const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
+const currencyEl = document.getElementById("currency");
 
 populateUI();
 
 let ticketPrice = +movieSelect.value;
+let rate;
+
+// Get exchange rate
+async function calculate() {
+  try {
+    let response = await fetch(
+      `https://api.exchangerate-api.com/v4/latest/USD`
+    );
+    let data = await response.json().then((data) => {
+      rate = data.rates[currencyEl.value];
+      // Change options rate
+      let options = [
+        `Avengers: Endgame `,
+        `Joker `,
+        `Toy Story 4 `,
+        `The Lion King `,
+      ];
+
+      options.forEach((element, key) => {
+        movieSelect[key] = new Option(
+          `${element} (${(movieSelect[key].value * rate).toFixed(2)} ${
+            currencyEl.value
+          })`,
+          movieSelect[key].value
+        );
+      });
+      total.innerText = count.innerText * (movieSelect.value * rate).toFixed(2);
+    });
+    return data;
+  } catch (error) {
+    alert(error);
+  }
+}
 
 // Save selected movie index and price
 function setMovieData(movieIndex, moviePrice) {
@@ -25,7 +59,8 @@ function updateSelectedCount() {
   const selectedSeatsCount = selectedSeats.length;
 
   count.innerText = selectedSeatsCount;
-  total.innerText = selectedSeatsCount * ticketPrice;
+  total.innerText =
+    selectedSeatsCount * (ticketPrice * (rate ? rate : 1)).toFixed(2);
 
   setMovieData(movieSelect.selectedIndex, movieSelect.value);
 }
@@ -48,6 +83,10 @@ function populateUI() {
     movieSelect.selectedIndex = selectedMovieIndex;
   }
 }
+
+//Exchanges events
+currencyEl.addEventListener("change", calculate);
+total.addEventListener("change", calculate);
 
 // Movie select event
 movieSelect.addEventListener("change", (e) => {
